3946faa4d731a3c3eed3320206336b7a186e3421
zhaoxiaoguang <zhaoxiaoguang@baidu.com>
2017-02-28 12:51:42 +0800

    v2

--- src/main/java/light/Main.java
+++ src/main/java/light/Main.java
@@ -1,24 +1,48 @@
+package light;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.util.*;
+import java.util.List;
 
 /**
  * Created by me on 2017/2/26.
  */
-public class Main {
+public class Main  extends JFrame implements ActionListener{
 
     static String[] titles = {"月","日","字","号","摘要","借方金额","贷方金额","对方科目"};
     static String[] outputTitles = {"日期","记账凭证","摘要","借方金额","贷方金额","对方科目"};
+    static int[] sheetWidths = {14, 8, 30, 12, 12, 20};
 
     public static void main(String[] args) throws Exception {
+        new Main().initGUI();
+    }
+
+    public static void backUpOriginFile(String path) {
+        Utils.copyFile(path, "doc/备份-示例1.xlsx");
+    }
+
+    public static void initProcess(File file) throws Exception {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("请输入需要处理的文件路径（例如：F:\\\\xg\\\\kaidigao\\\\doc\\\\示例.xls）：");
-//        String path = in.nextLine();
-        String path = "F:\\xg\\kaidigao\\doc\\示例.xls";
+        // 命令行方式读取文件路径=========
+        if (!file.exists() || !file.isFile()) {
+            System.out.println("请手动输入需要处理的文件路径（例如：F:\\xg\\kaidigao\\doc\\示例.xls）：");
+            String path = in.nextLine();
+            file = new File("D:\\work\\kaidigao\\doc\\示例1.xlsx");
+        }
+        // ===============================
 
         System.out.println("请输入需要处理的 sheet 的编号，从1开始，以空格间隔（例如：1 2 3）：");
         String sheetStr = in.nextLine();
@@ -26,42 +50,29 @@ public class Main {
         String[] strs = sheetStr.split(" ");
         int[] sheetIndexes = new int[strs.length];
         for (int i = 0; i < sheetIndexes.length; i++) {
-            sheetIndexes[0] = Integer.valueOf(strs[i]) - 1;
+            sheetIndexes[i] = Integer.valueOf(strs[i]) - 1;
         }
 
-//        backUpOriginFile(path);
-        func(path, sheetIndexes);
+        func(file, sheetIndexes);
     }
 
-    public static void backUpOriginFile(String path) {
-        Utils.copyFile(path, "doc/备份-示例1.xlsx");
-    }
-
-    public static void initGUI() {
-        //实例化一个JFrame类的对象
-        javax.swing.JFrame jf = new javax.swing.JFrame();
-        //设置窗体的标题属性
-        jf.setTitle("开底稿");
-        //设置窗体的大小属性
-        jf.setSize(250,350);
-        //设置窗体的位置属性
-        jf.setLocation(400,200);
-        //设置窗体关闭时退出程序
-        jf.setDefaultCloseOperation(3);
-        //设置禁止调整窗体的大小
-        jf.setResizable(false);
-        //设置窗体是否可见
-        jf.setVisible(true);
-    }
-
-    public static void func(String path, int[] sheetIndexes) throws Exception {
-        Workbook book = new XSSFWorkbook(path);
+    public static void func(File file, int[] sheetIndexes) throws Exception {
+        String fileName = file.getName();
+        String suffix = fileName.substring(fileName.length() - 4);
+        Workbook book = null;
+        if (suffix.equals(".xls")) {
+            book = new HSSFWorkbook(new POIFSFileSystem(new FileInputStream(file)));
+        } else if (suffix.equals("xlsx")) {
+            book = new XSSFWorkbook(file);
+        } else {
+            throw new Exception();
+        }
 
         for (int sheetIndex : sheetIndexes) {
 
             Sheet defaultSheet = book.getSheetAt(sheetIndex);
 
-            System.out.println("处理编号为：" + sheetIndex + "的sheet, sheet名为：" + defaultSheet.getSheetName());
+            System.out.println("处理编号为：" + (sheetIndex + 1) + " 的sheet, sheet名为：" + defaultSheet.getSheetName());
 
             // 查找表头所在 Cell
             HashMap<String, Cell> titleCells = new HashMap<>();
@@ -106,16 +117,24 @@ public class Main {
             Map<String, Integer> outputColIndex = new HashMap<>();
             for (int i = 0; i < outputTitles.length; i++) {
                 headRow.createCell(i).setCellValue(outputTitles[i]);
+                sheet.setColumnWidth(i, sheetWidths[i] * 256);
                 outputColIndex.put(outputTitles[i], i);
             }
 
 
             // 截取部分条目: number
-            System.out.println("保留金额最大的n条记录，请输入n值，（例如：20）：");
-            int number = in.nextInt();
+            System.out.println("保留金额最大的n条记录，0或者不输入表示全都保留，（例如：20）：");
+            in.nextLine();
+            String numberStr = in.nextLine();
+            int number;
+            if (numberStr == null || numberStr.trim().length() == 0) {
+                number = 0;
+            } else {
+                number = Integer.valueOf(numberStr.trim());
+            }
 
             double thred = 0;
-            if (valids.size() > number) {
+            if (number != 0 && valids.size() > number) {
                 List<Double> moneys = new ArrayList<>();
                 for (Map<String, Cell> tmp : valids) {
                     moneys.add(tmp.get("借方金额").getNumericCellValue());
@@ -178,9 +197,11 @@ public class Main {
         }
 
         // 保存文件
-        OutputStream out = new FileOutputStream(path + "A");
+        OutputStream out = new FileOutputStream(file.getParent() + "\\" + "已处理-" + file.getName());
         book.write(out);
-        System.out.println("完成！");
+        out.close();
+        System.out.print("完成！");
+        System.out.println("保存为：" + file.getParent() + "\\" + "已处理-" + file.getName());
     }
 
     public static Cell findCell(Sheet sheet, String title) {
@@ -196,5 +217,44 @@ public class Main {
             }
         }
         return null;
+    }
+
+    JFileChooser fc = new JFileChooser();//创建文件对话框对象
+    JButton open;
+
+    public void initGUI()
+    {
+        Container container = this.getContentPane();
+        container.setLayout(new FlowLayout());
+        this.setTitle("打开底稿文件");
+        open = new JButton("打开文件");
+        open.addActionListener(this);
+        container.add(open);//添加到内容窗格上
+        this.setVisible(true);
+        this.setSize(200,80);
+
+    }
+
+    public void actionPerformed(ActionEvent e)
+    {
+        JButton button = (JButton)e.getSource();//得到事件源
+        if(button == open)//选择的是“打开文件”按钮
+        {
+            int select = fc.showOpenDialog(this);//显示打开文件对话框
+            if(select == JFileChooser.APPROVE_OPTION)//选择的是否为“确认”
+            {
+                File file = fc.getSelectedFile();
+                System.out.println("选择文件："+file.getName());
+                try {
+                    initProcess(file);
+                } catch (Exception ex) {
+                    System.out.println("处理出现错误！");
+                    ex.printStackTrace();
+                }
+                System.out.println("请选择下一个需要处理的文件！");
+            }
+            else
+                System.out.println("操作被取消");
+        }
     }
 }
